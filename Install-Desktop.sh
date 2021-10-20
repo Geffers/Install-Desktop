a4f4f5d84b36789e409d7646e94428fede6c7fd1
Geoff Lane <xray850@gmail.com>
2021-10-20 08:06:53 +0100

    Create Install-Desktop.sh

---
+++ Install-Desktop.sh
@@ -0,0 +1,59 @@
+#! /bin/bash
+
+# Script for updating, upgrading of Buster Lite
+# Will also install Desktop and RealVNC then enable it
+# Created 19 October 2021
+
+clear
+echo "Script for updating and upgrading Buster Lite"
+sleep 5s
+echo "Will also install Desktop, Real VNC and enable both"
+sleep 5s
+
+
+echo "Doing and running update"
+sleep 5s
+echo "Be patient"
+sudo apt update -y
+sleep 5s
+clear
+sleep 10s
+echo "Now updating and upgrading packages, could take some time"
+echo "Get yourself a coffee"
+sleep 10s
+clear
+echo "system is now updating packages."
+sudo apt upgrade -y
+clear
+sleep 10s
+echo "Right, system now up to date"
+sleep 10s
+clear
+echo "Now to install the desktop"
+echo "  "
+echo "First the x-server"
+sudo apt install xserver-xorg -y
+clear
+sleep 10s
+echo "Now the actual Desktop"
+sleep 10s
+echo "This may take some time as lots of items to download"
+echo "  "
+echo "Get another coffee"
+sudo apt install raspberrypi-ui-mods -y
+clear
+clear
+echo "Done, nearly there"
+echo "  "
+echo "now to install the VNC-Server"
+sleep 10s
+sudo apt install realvnc-vnc-server -y
+sudo systemctl enable vncserver-x11-serviced.service
+sleep 10s
+sudo systemctl start vncserver-x11-serviced.service 
+clear
+sudo systemctl start lightdm.service
+echo "ALL DONE"
+echo "  "
+echo "Let's reboot and see if it works"
+sudo reboot
